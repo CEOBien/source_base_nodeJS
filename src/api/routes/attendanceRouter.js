4251f7f412e272655494a7aca391be4ae0528133
duy.phan <duy.phan@novasquare.vn>
2023-07-11 11:20:49 +0700

    fix merge err

--- src/api/routes/attendanceRouter.js
+++ src/api/routes/attendanceRouter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const attendanceController = require("../controllers/attendanceController");
 const router = require("express").Router();
 
@@ -9,15 +8,3 @@ router.get("/get/:id", attendanceController.getUserId);
 router.delete("/delete/:id", attendanceController.deleteAttendance);
 
 module.exports = router;
-=======
-const attendanceController = require("../controllers/attendanceController");
-const router = require("express").Router();
-
-router.post("/checkin", attendanceController.Checkin);
-router.post("/checkout", attendanceController.Checkout);
-router.get("/getall", attendanceController.getAllByQuery);
-router.get("/get/:id", attendanceController.getUserId);
-router.delete("/delete/:id", attendanceController.deleteAttendance);
-
-module.exports = router;
->>>>>>> 940cfa322083c1553083e112954f2c7a261e60d3
